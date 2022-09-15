3fbf9b559ee2339f74058945d96deaf9369562b1
Roy Chuang <roy0702.chuang@gmail.com>
2022-09-15 22:24:13 +0800

    InsertionSort

---
+++ WindowsPowerShell/.ipynb_checkpoints/Microsoft.PowerShell_profile-checkpoint.ps1
@@ -0,0 +1,14 @@
+clear
+
+cd C:\msys64\home\s0415
+$BASH = 1
+
+Set-alias -n "npp" "C:\Users\s0415\AppData\Local\Programs\npp\notepad++.exe"
+if ($BASH) {
+    Set-alias -n "ls" "C:\msys64\usr\bin\ls"  -Option AllScope
+    Set-alias -n "man" "C:\msys64\usr\bin\man"  -Option AllScope
+}
+function Prompt
+{
+    "$([char]27)[0mRoyChuang $([char]27)[31m" + "::" + "$([char]27)[32m $((Get-Location).Path)$([char]27)[34m" + "`n>> $([char]27)[0m"
+}
