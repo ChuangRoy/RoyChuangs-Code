83c036aba2c422cab7c1b3e054b1e7bf641312df
Roy Chuang <roy0702.chuang@gmail.com>
2022-09-17 21:41:10 +0800

    finish insertion sort project

--- C_CPP/CBProjects/InsertionSort/InsertionSort.cpp
+++ C_CPP/CBProjects/InsertionSort/InsertionSort.cpp
@@ -3,9 +3,9 @@
 #include <cstdlib>
 using namespace std;
 
-int list_length = 5;
-int l[5] = {60, 50, 44, 82, 55};
 
+
+  /*
 void random_list () {
     srand (time(NULL));
     for (int i = 0; i < list_length; i++) {
@@ -13,32 +13,25 @@ void random_list () {
         // cout << l[i] << endl;
     }
 }
-
-void insert_into_list (int position, int target) { // Insert target in front of position
-    int backup[list_length];
-    for (int i = 0; i < list_length; i++) //copy list
-        backup[i] = l[i];
-    for (int i = position; i < target; i++) // every index + 1
-        l[i+1] = backup[i];
-    l[position] = backup[target];
-    // debug
-    for (int i = 0; i < list_length; i++)
-        cout << "[DEBUG]: l[" << i << "] = " << l[i] << endl;
-}
+ */
 
 int main () {
+    int list_length = 5;
+    int l[list_length] = {60, 50, 44, 82, 55};
     // random_list();
-    int last_index = 0;
-    bool sort_finish = 0;
-    while (!sort_finish) { //sort
-        int target_index = last_index + 1;
-        for (int i = 0; i <= last_index; i++) {
-            for (int i = target_index; i < 0; i--)
-                if (l[i] <ｌ[i-1])
-                    int temp = l[i];l[i] = l[]
+    for(int last_index = 0; last_index < list_length - 1; last_index++) { //sort
+        for (int target_index = last_index + 1; target_index >= 0; target_index--) { // comparing from last index to 0
+            if (l[target_index] < l[target_index-1]) {
+                int temp = l[target_index-1];
+                l[target_index-1] = l[target_index];
+                l[target_index] = temp;
+            }
         }
+   }
 
-    }
 
-
+    // print list
+    for (int i = 0; i < list_length; i++)
+        cout << l[i] << " ";
+    cout << endl;
 }
