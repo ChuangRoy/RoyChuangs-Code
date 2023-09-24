492161a446c8db97c861520d488e80d3ba197005
Replit user <>
2023-09-24 12:19:28 +0000

    .

--- personal_files_hidden/WindowsPowerShell/activate_mingw.ps1
+++
@@ -1,9 +0,0 @@
-$script:THIS_PATH = $myinvocation.mycommand.path
-$script:BASE_DIR = Split-Path (Resolve-Path "$THIS_PATH") -Parent
-
-function global:prompt
-    {
-    "$([char]27)[32m$((Get-Location).Path)" + "`n>>"
-    }
-
-$env:PATH = "$BASE_DIR/mingw64/bin;" + $env:PATH
