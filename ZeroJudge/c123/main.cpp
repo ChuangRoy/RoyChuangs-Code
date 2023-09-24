e91d4371b16cf09b6e756ab925b9918bf1ea3ae8
Replit user <>
2023-09-24 09:37:19 +0000

    .

--- ZeroJudge/c123/main.cpp
+++ ZeroJudge/c123/main.cpp
@@ -2,12 +2,34 @@
 using namespace std;
 
 int main () {
-    int m, n;
-    while (cin >> m) {
-        cin >> n;
-        if (n == 0){
-            continue;
+    int M, N;
+    cin >> N;
+    while (N) {
+        queue<int> a;
+        cin >> M;
+        while (M) {
+            a.push(M);
+            int temp;
+            for (int i = 1; i < N; i++) {
+                cin >> temp;
+                a.push(temp);
+            }
+            ///////
+            stack<int> A_way, Stat, B_way;
+            bool ac = 1;
+            for (int i = N; i > 0; i++) {
+                A_way.push(i);
+            }
+            while ( (!A_way.empty()) && (ac)) {
+                temp = a.pop();
+                if (temp > A_way.top()) {
+                    while (temp > A_way.top)
+                }
+            }
+            ///////
+            cin >> M;
         }
-
+        cin >> N;
     }
+    
 }
