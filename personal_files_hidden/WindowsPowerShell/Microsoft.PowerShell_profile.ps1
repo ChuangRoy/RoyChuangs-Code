8de0b19e629050b50aea68d04a9865e1611aca74
Replit user <>
2023-10-09 14:08:14 +0000

    .

--- personal_files_hidden/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ personal_files_hidden/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,1 @@
-clear
-Set-alias -n "npp" "C:\Program Files\Notepad++\notepad++.exe"
-
-function Prompt
-{
-    "$([char]27)[0mRoyChuang $([char]27)[31m" + "::" + "$([char]27)[32m $((Get-Location).Path)$([char]27)[34m" + "`n>> $([char]27)[0m"
-}
+pwsh -WorkingDirectory "$((Get-Location).Path)"
