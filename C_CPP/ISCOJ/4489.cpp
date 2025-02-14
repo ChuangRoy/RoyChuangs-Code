15d9aeb8f4df1d8eafa0b7eb9cb416d949e11fb6
Roy Chuang <roy0702.chuang@gmail.com>
2025-02-14 19:38:42 +0800

    19:38 2/14/2025

--- C_CPP/ISCOJ/4489.cpp
+++ C_CPP/ISCOJ/4489.cpp
@@ -1,22 +1,37 @@
 #include <bits/stdc++.h>
+
+#define int long long
+#define AC ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+
 using namespace std;
+using ll = long long;
 const int mod = 1e9+7;
 
-int main () {
-	ios_base::sync_with_stdio(false);cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	if (n - m < m)
-		m = n - m;
-	unsigned long long a = 1;
-	// unsigned long long b = 1;
-	for (int i = n; i > n - m; i--) {
-		a = (a * i) % mod;
-	}
-	for (int i = 1; i <= m; i++) {
-		while (a % i) a += mod;
-		a = a / i;
-	}
-	// while (a % b) a += mod;
-	cout << a % mod << endl;
+int qpow (int a, int b, int p)
+{
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % p;
+        b >>= 1;
+        a = a * a % p;
+    }
+    return res;
+}
+
+signed main ()
+{
+    AC
+    int n, m;
+    cin >> n >> m;
+    int a = 1;
+    m = min(m, n - m);
+    for (int i = n; i > n - m; i--) {
+        a = (a * i) % mod;
+    }
+    for (int i = 1; i <= m; i++) {
+        int mi = qpow(i, mod - 2, mod);
+        a = (a * mi) % mod;
+    }
+    cout << a << '\n';
+    return 0;
 }
