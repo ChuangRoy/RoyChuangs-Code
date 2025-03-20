af58281e9849006c1e1b36858b73a96732ca670d
Roy Chuang <roy0702.chuang@gmail.com>
2025-03-20 23:23:04 +0800

    23:23 3/20/2025

--- C_CPP/CSES/Introductory Problems/Gray Code.cpp
+++ C_CPP/CSES/Introductory Problems/Gray Code.cpp
@@ -1,25 +1,12 @@
 #pragma GCC optimize("Ofast")
-
 #include <bits/stdc++.h>
+#define AC ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+#define ALL(x) begin(x),end(x)
+#define pb emplace_back
 
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
-
-/* Template */
-#define AC ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
-#define ALL(x) begin(x),end(x)
-#define pb emplace_back
-#ifdef LOCAL
-#define debug(args...) LKJ("[ " #args " ]", args)
-template<class I> void LKJ(I&&x) { cerr << x << '\n'; }
-template<class I, class...T> void LKJ(I&&x, T&&...t) { cerr << x << ", ", LKJ(t...); }
-template<class I> void print(I a, I b) { while(a < b) cerr << *a << " \n"[next(a) == b], ++a; }
-#else
-#define debug(...) 0
-#define print(...) 0
-#endif
-/* End of Template */
 
 vector<string> gcode (int n)
 {
